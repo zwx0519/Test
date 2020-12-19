ece63ff0b88c52a93d2368c470b324cb1d66d341
zwx0519 <15564732161@163.com>
2020-12-19 10:59:36 +0800

    专题

--- app/src/main/java/com/example/test/ui/shop/special/SpecialFragment.java
+++ app/src/main/java/com/example/test/ui/shop/special/SpecialFragment.java
@@ -1,9 +1,12 @@
 package com.example.test.ui.shop.special;
 
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,17 +22,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
+
 //TODO 专题内容
 public class SpecialFragment extends BaseFragment<ISpecial.Presenter> implements ISpecial.View {
     @BindView(R.id.mRlv_special)
     RecyclerView mRlv;
+    @BindView(R.id.iv_special_all)
+    ImageView iv_All;
+    @BindView(R.id.tv_special_oading)
+    TextView tv_lOading;
     @BindView(R.id.btn_previous_page)
-    Button btn_Previous_Page;
+    RadioButton btn_PreviousPage;
     @BindView(R.id.btn_next_page)
-    Button btn_Next_Page;
+    RadioButton btn_NextPage;
+    @BindView(R.id.mNsv_special)
+    NestedScrollView mNsv_Special;
     private ArrayList<SpecialBean.DataBeanX.DataBean> list;
     private SpecialAdapter specialAdapter;
-    private int page=1;
+    private int ONE = 1;
+    private int TWO = 2;
+    private int page = ONE;
 
     @Override
     protected int getLayout() {
@@ -43,13 +56,6 @@ public class SpecialFragment extends BaseFragment<ISpecial.Presenter> implements
 
     @Override
     protected void initView() {
-        btn_Previous_Page.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
-
         mRlv.setLayoutManager(new LinearLayoutManager(getContext()));
         mRlv.addItemDecoration(new ItemDecoration(getContext()));
         list = new ArrayList<>();
@@ -64,8 +70,45 @@ public class SpecialFragment extends BaseFragment<ISpecial.Presenter> implements
 
     @Override
     public void getSpecialReturn(SpecialBean result) {
+        //清空集合
+        list.clear();
         List<SpecialBean.DataBeanX.DataBean> data = result.getData().getData();
         list.addAll(data);
         specialAdapter.notifyDataSetChanged();
+
+        //隐藏加载中...
+        iv_All.setVisibility(View.GONE);
+        tv_lOading.setVisibility(View.GONE);
     }
+
+//    @OnClick({R.id.btn_previous_page, R.id.btn_next_page})
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.btn_previous_page:
+//                //更换page页 默认第一页
+//                page = ONE;
+//                //显示加载中...      白版
+//                iv_All.setVisibility(View.VISIBLE);
+//                tv_lOading.setVisibility(View.VISIBLE);
+//
+//                //请求数据
+//                presenter.getSpecial(page);
+//
+//                // 返回顶部 自动去最上面
+//                mNsv_Special.fullScroll(ScrollView.FOCUS_UP);
+//                break;
+//            case R.id.btn_next_page:
+//                //更换page页
+//                page = TWO;
+//
+//                //显示加载中...  白板
+//                iv_All.setVisibility(View.VISIBLE);
+//                tv_lOading.setVisibility(View.VISIBLE);
+//                //请求数据
+//                presenter.getSpecial(page);
+//                // 返回顶部
+//                mNsv_Special.fullScroll(ScrollView.FOCUS_UP);
+//                break;
+//        }
+//    }
 }
